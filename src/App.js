0ccb26b04e24299fd89b79413990c3d97c35cd22
Nick Cox <nick@nickcox.me>
2021-06-17 21:23:52 -0700

    Get increment and decrement working

--- src/App.js
+++ src/App.js
@@ -1,64 +1,88 @@
 import React from "react";
 import "./styles.css";
 import AnimatedNumber from "react-animated-number";
+import config from "./config";
+import map from "lodash.map";
+import reduce from "lodash.reduce";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.decrement = this.decrement.bind(this);
     this.increment = this.increment.bind(this);
+    console.log(config.budget);
+    const remainingTotal = reduce(
+      config.budget.items,
+      (accum, item) => {
+        return accum - item.count * item.cost;
+      },
+      config.budget.total
+    );
     this.state = {
+      ...config,
       budget: {
-        remaining: 500,
-        total: 500,
-        items: [
-          {
-            name: "Give employees healthcare",
-            cost: 100
-          }
-        ]
+        ...config.budget,
+        total: remainingTotal
       }
     };
   }
 
-  decrement(cost) {
+  decrement(item, id) {
     this.setState((prevState) => ({
       ...prevState,
       budget: {
         ...prevState.budget,
-        remaining: prevState.budget.remaining - cost
+        total: prevState.budget.total + item.cost,
+        items: {
+          ...prevState.budget.items,
+          [id]: {
+            ...item,
+            count: item.count - 1
+          }
+        }
       }
     }));
   }
 
-  increment(cost) {
+  increment(item, id) {
     this.setState((prevState) => ({
       ...prevState,
       budget: {
         ...prevState.budget,
-        remaining: prevState.budget.remaining + cost
+        total: prevState.budget.total - item.cost,
+        items: {
+          ...prevState.budget.items,
+          [id]: {
+            ...item,
+            count: item.count + 1
+          }
+        }
       }
     }));
   }
 
   render() {
+    console.log(this.state.budget);
     return (
       <div className="App">
         <h1>Spend the budget</h1>
         <h3>
           Remaining:{" "}
           <AnimatedNumber
-            value={this.state.budget.remaining}
-            formatValue={(v) => `$${v.toFixed(0)}`}
+            value={this.state.budget.total}
+            formatValue={(v) =>
+              `$${v.toLocaleString("en-US", { minimumFractionDigits: 0 })}`
+            }
             duration={300}
+            stepPrecision={0}
           />
         </h3>
-        {this.state.budget.items.map((i) => {
+        {map(this.state.budget.items, (i, id) => {
           return (
-            <li>
-              {i.name}
-              <button onClick={() => this.increment(i.cost)}>Remove</button>
-              <button onClick={() => this.decrement(i.cost)}>Add</button>
+            <li key={i.name}>
+              {i.name}: {i.count}
+              <button onClick={() => this.decrement(i, id)}>Remove</button>
+              <button onClick={() => this.increment(i, id)}>Add</button>
             </li>
           );
         })}
---
+++ src/config.js
@@ -0,0 +1,29 @@
+const config = {
+  budget: {
+    total: 250000000,
+    items: {
+      1: {
+        name: "Give employees healthcare",
+        cost: 100000,
+        count: 0
+      },
+      2: {
+        name: "Scholarship Programs for Students of Color",
+        cost: 1000000,
+        count: 0
+      },
+      3: {
+        name: "Cost of living pay increase for all staff and faculty",
+        cost: 10000000,
+        count: 0
+      },
+      4: {
+        name: "New atheletic facility",
+        cost: 40000000,
+        count: 2
+      }
+    }
+  }
+};
+
+export default config;
