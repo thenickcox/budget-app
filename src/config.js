d635ecc8914fb8e5b1e50bef9f7bc07c69bc1a2a
Nick Cox <nick@nickcox.me>
2021-07-29 22:50:23 -0700

    Add new config categories

--- src/config.js
+++ src/config.js
@@ -35,6 +35,41 @@ const config = {
         name: "International Student Affairs",
         cost: 1000000,
         count: 1
+      },
+      7: {
+        name: "New Engineering Building",
+        cost: 73000000,
+        count: 1
+      },
+      8: {
+        name: "Business School Renovation",
+        cost: 5000000,
+        count: 1
+      },
+      9: {
+        name: "Auraria Campus Police Department: Annual",
+        cost: 5400000,
+        count: 1
+      },
+      10: {
+        name: "House a student for a year",
+        cost: 20000,
+        count: 10
+      },
+      11: {
+        name: "Feed a student for a year",
+        cost: 4080,
+        count: 10
+      },
+      12: {
+        name: "Full Ride Scholarship (4 years)",
+        cost: 31680,
+        count: 10
+      },
+      13: {
+        name: "Childcare for a year",
+        cost: 15325,
+        count: 5
       }
     }
   }
